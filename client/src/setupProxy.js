b6588d3da3db7299d2a9df093830712adb76143e
kyomin <kim031504@naver.com>
2020-07-30 17:25:57 +0900

    cors 이슈 해결을 위한 proxy 설정 완료

---
+++ client/src/setupProxy.js
@@ -0,0 +1,13 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+// 클라이언트 요청 : /api/**
+// 실제 : http://localhost:5000/api/**
+module.exports = function(app) {
+  app.use(
+    '/api',
+    createProxyMiddleware({
+      target: 'http://localhost:5000',
+      changeOrigin: true,
+    })
+  );
+}; 
