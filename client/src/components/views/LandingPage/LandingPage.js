60d1143c97575aed82d8162a8a7bf905a123bd5c
kyomin <kim031504@naver.com>
2020-07-31 17:01:53 +0900

    로그아웃 기능 구현 완료

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,11 +1,24 @@
 import React, { useEffect } from 'react';
 import axios from 'axios';
 
-function LandingPage() {
+function LandingPage(props) {
 
     useEffect(() => {
 
     }, []);
+
+    const onLogoutHandler = () => {
+        axios.get('/api/user/logout')
+        .then(res => {
+            console.log('응답 데이터 : ', res.data);
+            if(res.data.logoutSuccess) {
+                alert('로그아웃 되었습니다.');
+                props.history.push('/login');
+            } else {
+                alert('로그아웃에 실패했습니다.');
+            }
+        })
+    }
 
     return (
         <div style={{
@@ -13,6 +26,10 @@ function LandingPage() {
             width: '100%', height: '100vh'
         }}>
             <h2>LandingPage</h2>
+
+            <button onClick={onLogoutHandler}>
+                로그아웃
+            </button>
         </div>
     )
 }
